f7c432693f37ca80f4c30e1b4878f79767dc6778
Sandy Kee <sandykee95@hotmail.com>
2019-11-10 20:19:34 +0800

    Update
    Update the codes

--- testCase.py
+++ testCase.py
@@ -1,13 +1,15 @@
 import os
 import time
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+#from rp-portfolio.personal_portfolio import *
 
-#from selenium.webdriver.common.by import By
+#----------------No Need--------------------------------
 #from selenium.common.exceptions import TimeoutException
-
 #from selenium.webdriver.support.select import Select
 
 def test_open():
@@ -23,8 +25,6 @@ def test_scrolling():
     driver.maximize_window()
     driver.get('http://localhost:8000/projects/')
     driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-    #html = driver.find_element_by_tag_name('html')
-    #html.send_keys(Keys.END)
     time.sleep(2)
     driver.quit()
 
@@ -54,7 +54,57 @@ def test_blog_CCAactivities():
     yourName.send_keys("Test")
     enterComment = driver.find_element_by_id('id_body')
     enterComment.send_keys("Good Job")
-    button = driver.find_element_by_link_text("Submit")
+    button = driver.find_element_by_class_name("btn.btn-primary")
     button.click()
+    driver.implicitly_wait(10)
+    text = driver.find_element_by_tag_name("p").text
+    with pytest.raises(AssertionError):
+        assert text == "Good Job"
+    print("Not found")
+    driver.quit()
     
 test_blog_CCAactivities()
+    
+def test_blog_Photography():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get('http://localhost:8000/projects/')
+    elem = driver.find_element_by_link_text('Blog')
+    elem.click()
+    time.sleep(2)
+    elem = driver.find_element_by_link_text('Photography')
+    elem.click()
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'id_author')))
+    yourName = driver.find_element_by_id('id_author')
+    yourName.send_keys("Test")
+    enterComment = driver.find_element_by_id('id_body')
+    enterComment.send_keys("Good Job")
+    button = driver.find_element_by_class_name("btn.btn-primary")
+    button.click()
+    driver.implicitly_wait(10)
+    #assert
+    driver.quit()
+    
+#test_blog_Photography()
+
+def test_blog_Project():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get('http://localhost:8000/projects/')
+    elem = driver.find_element_by_link_text('Blog')
+    elem.click()
+    time.sleep(2)
+    elem = driver.find_element_by_link_text('Purple Project')
+    elem.click()
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'id_author')))
+    yourName = driver.find_element_by_id('id_author')
+    yourName.send_keys("Test")
+    enterComment = driver.find_element_by_id('id_body')
+    enterComment.send_keys("Good Job")
+    button = driver.find_element_by_class_name("btn.btn-primary")
+    button.click()
+    driver.implicitly_wait(10)
+    #assert
+    driver.quit()
+    
+#test_blog_Project()
